f844232fa699a005b634b06a8d61a40361f3ca52
horita-yuya <horitayuya@gmail.com>
2024-02-20 13:03:39 +0900

    Add sample

--- Suspense/Package.swift
+++ Suspense/Package.swift
@@ -7,14 +7,11 @@ let package = Package(
     name: "Suspense",
     platforms: [.iOS(.v16)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Suspense",
             targets: ["Suspense"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Suspense"),
         .testTarget(
---
+++ SuspenseExample/SuspenseExample/SuspenseExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuspenseExampleApp.swift
+//  SuspenseExample
+//
+//  Created by Yuya Horita on 2024/02/20.
+//
+
+import SwiftUI
+
+@main
+struct SuspenseExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
