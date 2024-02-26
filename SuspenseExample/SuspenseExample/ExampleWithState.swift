5cc5d50a2ffe06b8dc60b40b504f6027a32653ae
horita-yuya <horitayuya@gmail.com>
2024-02-26 16:53:07 +0900

    Add resolve component testing

--- SuspenseExample/SuspenseExample/ExampleWithState.swift
+++ SuspenseExample/SuspenseExample/ExampleWithState.swift
@@ -1,12 +1,39 @@
 import Suspense
 import SwiftUI
 
+struct ExampleWithInternalError: View {
+    enum Error: Swift.Error {
+        case `internal`
+    }
+
+    init(value: String?) throws {
+        _ = try use(value) {
+            try await Task.sleep(nanoseconds: 1_000_000_000)
+            return "Hello"
+        }
+
+        throw Error.internal
+    }
+
+    var body: some View {
+        Text("Hello")
+    }
+}
+
 struct ExampleWithState: View {
     @State private var count: Int = 0
 
     var body: some View {
         Suspense {
             try ThrowableCounter(name: $0, count: count)
+        }
+
+        ErrorBoundary {
+            Suspense {
+                try ExampleWithInternalError(value: $0)
+            }
+        } fallback: { _, _ in
+            Text("Internal error happened")
         }
 
         SimpleCounter(count: count)
