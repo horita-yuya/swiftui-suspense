b82971a90512c87863f861d511f2f918d6f971df
horita-yuya <horitayuya@gmail.com>
2024-02-26 12:51:16 +0900

    Separate async throws vs throws

--- Sources/Suspense/Suspense.swift
+++ Sources/Suspense/Suspense.swift
@@ -1,12 +1,21 @@
 import SwiftUI
 
 public struct Suspense<PAGE: View, A>: View {
-    enum Component {
-        case voidComponent(() async throws -> PAGE)
-        case component((A?) async throws -> PAGE)
+    fileprivate typealias ThrowableComponent = (A?) throws -> PAGE
+    fileprivate typealias AsyncComponent = () async throws -> PAGE
+
+    private enum Component {
+        case asyncComponent(AsyncComponent)
+        case throwableComponent(ThrowableComponent)
     }
 
-    @State private var page: PAGE?
+    private enum Status {
+        case pending
+        case completedAsync(PAGE)
+        case completedThrowable(A)
+    }
+
+    @State private var status: Status = .pending
     @Environment(ErrorHandler.self) private var errorHandler: ErrorHandler?
     private var component: Component
     private var fallback: AnyView
@@ -16,7 +25,7 @@ public struct Suspense<PAGE: View, A>: View {
         @ViewBuilder component: @escaping () async throws -> PAGE,
         onError: ((Error) -> Void)? = nil
     ) where A == Never {
-        self.component = .voidComponent(component)
+        self.component = .asyncComponent(component)
         self.fallback = AnyView(ProgressView())
         self.onError = onError
     }
@@ -26,16 +35,16 @@ public struct Suspense<PAGE: View, A>: View {
         @ViewBuilder fallback: () -> some View,
         onError: ((Error) -> Void)? = nil
     ) where A == Void {
-        self.component = .voidComponent(component)
+        self.component = .asyncComponent(component)
         self.fallback = AnyView(fallback())
         self.onError = onError
     }
 
     public init(
-        @ViewBuilder component: @escaping (A?) async throws -> PAGE,
+        @ViewBuilder component: @escaping (A?) throws -> PAGE,
         onError: ((Error) -> Void)? = nil
     ) {
-        self.component = .component(component)
+        self.component = .throwableComponent(component)
         self.fallback = AnyView(ProgressView())
         self.onError = onError
     }
@@ -45,45 +54,64 @@ public struct Suspense<PAGE: View, A>: View {
         @ViewBuilder fallback: () -> some View,
         onError: ((Error) -> Void)? = nil
     ) {
-        self.component = .component(component)
+        self.component = .throwableComponent(component)
         self.fallback = AnyView(fallback())
         self.onError = onError
     }
 
     public var body: some View {
-        if let page = page {
-            page
-        } else {
+        switch status {
+        case .pending:
             fallback
                 .task {
                     do {
                         switch component {
-                        case .voidComponent(let component):
-                            page = try await component()
-                        case .component(let component):
-                            page = try await component(nil)
-                        }
+                        case .asyncComponent(let component):
+                            status = .completedAsync(try await component())
 
-                    } catch Promise<A>.pending(let query) {
-                        do {
-                            let data = try await query()
-                            switch component {
-                            case .voidComponent(let component):
-                                page = try await component()
+                        case .throwableComponent(let component):
+                            do {
+                                _ = try component(nil)
+                            } catch Promise<A>.pending(let query) {
+                                do {
+                                    let data = try await query()
+                                    status = .completedThrowable(data)
 
-                            case .component(let component):
-                                page = try await component(data)
+                                } catch {
+                                    onError?(error)
+                                    errorHandler?.error = error
+                                }
                             }
-
-                        } catch {
-                            onError?(error)
-                            errorHandler?.error = error
                         }
                     } catch {
                         onError?(error)
                         errorHandler?.error = error
                     }
                 }
+        case .completedAsync(let page):
+            page
+
+        case .completedThrowable(let data):
+            switch component {
+            case .asyncComponent:
+                EmptyView()
+                    .onAppear {
+                        assertionFailure("AsyncComponent must not be completed as throwable")
+                    }
+
+            case .throwableComponent(let component):
+                resolveThrowableComponent(component: component, data: data)
+            }
+        }
+    }
+
+    private func resolveThrowableComponent(component: ThrowableComponent, data: A) -> AnyView {
+        do {
+            return AnyView(try component(data))
+        } catch {
+            onError?(error)
+            errorHandler?.error = error
+            return fallback
         }
     }
 }
